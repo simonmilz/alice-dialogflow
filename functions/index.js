87f58d124ca48df7c4948c0fe60a2af01085fa42
Simon Milz <smilz@nvidia.com>
2018-04-10 13:29:16 +0200

    removed obsolete code

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const functions = require('firebase-functions');
-const { WebhookClient } = require('dialogflow-fulfillment');
 const Queue = require('bull');
 
 const config = functions.config();
@@ -10,18 +9,15 @@ const JamesQueue = new Queue(config.queue.name, config.queue.uri);
 process.env.DEBUG = 'dialogflow:debug';
 
 exports.jamesFulfillment = functions.https.onRequest((request, response) => {
-	const agent = new WebhookClient({ request, response });
-
 	console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
 	console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
 
 	if ('james.tv' === request.body.result.action) {
 		return JamesQueue.add(request.body).then(function() {
-			agent.add(request.body.result.fulfillment.speech);
+			response.send({
+				speech: request.body.result.fulfillment.speech,
+				text: request.body.result.fulfillment.speech
+			});
 		});
-	} else {
-		agent.add('Ich verstehe deine Frage leider nicht.');
-		agent.add('Entschuldige bitte, ich habe deine Frage nicht verstanden.');
-		agent.add('Ich bin nicht so sicher, ob ich dich richtig verstanden habe.');
 	}
 });
