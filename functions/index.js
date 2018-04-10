dbb5e8a1c846d1fbca01a7b6607b59dc1d542e14
Simon Milz <smilz@nvidia.com>
2018-04-10 13:17:46 +0200

    removed commented code

--- functions/index.js
+++ functions/index.js
@@ -11,35 +11,17 @@ process.env.DEBUG = 'dialogflow:debug';
 
 exports.jamesFulfillment = functions.https.onRequest((request, response) => {
 	const agent = new WebhookClient({ request, response });
+
 	console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
 	console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
 
-	function welcome(agent) {
-		agent.add(`Welcome to my agent!`);
+	if ('james.tv' === request.body.result.action) {
+		return JamesQueue.add(request.body).then(function() {
+			agent.add(request.body.result.fulfillment.speech);
+		});
+	} else {
+		agent.add('Ich verstehe deine Frage leider nicht.');
+		agent.add('Entschuldige bitte, ich habe deine Frage nicht verstanden.');
+		agent.add('Ich bin nicht so sicher, ob ich dich richtig verstanden habe.');
 	}
-
-	function fallback(agent) {
-		agent.add(`I didn't understand`);
-		agent.add(`I'm sorry, can you try again?`);
-	}
-
-	// Uncomment and edit to make your own intent handler
-	// uncomment `intentMap.set('your intent name here', yourFunctionHandler);`
-	// below to get this function to be run when a Dialogflow intent is matched
-	function sendToQueue(agent) {
-		JamesQueue.add(request.body);
-		agent.add(request.body.result.fulfillment.speech);
-	}
-
-	// Run the proper function handler based on the matched Dialogflow intent name
-	let intentMap = new Map();
-
-	['sony.power.on', 'sony.power.off'].forEach(function(intent) {
-		intentMap.set(intent, sendToQueue);
-	});
-
-	intentMap.set('Default Welcome Intent', welcome);
-	intentMap.set('Default Fallback Intent', fallback);
-
-	agent.handleRequest(intentMap);
 });
